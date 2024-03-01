fbcdf1c37749bef57c424d45e202efee1a9374e8
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-01 10:01:37 +0600

    Service part update

--- src/Component/Service/Service.jsx
+++ src/Component/Service/Service.jsx
@@ -5,11 +5,13 @@ import Button from "../Common/Button";
 const Service = () => {
   return (
     <>
-      <div>
+      <div className="service">
         <div className="container">
           <div>
             <div className="pb-4">
-              <Title>Let’s try our service now!</Title>
+              <Title className={"service-title"}>
+                Let’s try our service now!
+              </Title>
               <p className="text-white opacity-50 text-xs leading-6 pt-4">
                 Everything you need to accept card payments and grow your
                 business anywhere on the planet.
