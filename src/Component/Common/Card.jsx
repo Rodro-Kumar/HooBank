3fe885ee1f01182222b0d0c5ba905b7d3a954cf1
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-01 09:59:23 +0600

    card update

--- src/Component/Common/Card.jsx
+++ src/Component/Common/Card.jsx
@@ -3,7 +3,7 @@ import png from "../../assets/“.png";
 import manimg from "../../assets/man1.png";
 import Flex from "./Flex";
 
-const Card = ({ className }) => {
+const Card = ({ className, cardDes }) => {
   return (
     <div
       className={` py-10 px-6 rounded-3xl border border-solid border-[#ffffff3a] ${className}`}
@@ -11,8 +11,9 @@ const Card = ({ className }) => {
       <div className="all-content">
         <img src={png} alt={png} className="mb-8" />
         <p className="pb-5 text-xs font-poppins font-normal text-white leading-5">
-          Money is only a tool. It will take you wherever you wish, but it will
-          not replace you as the driver.
+          {cardDes
+            ? cardDes
+            : " Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver."}
         </p>
         <Flex className={"flex-col gap-y-3"}>
           <div>
