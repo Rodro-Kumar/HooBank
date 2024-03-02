6d0b3acda2cd7fbd436f17218af1527db44efa3d
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-02 18:03:23 +0600

    Hero update

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -21,16 +21,26 @@ const Hero = () => {
                   <span className="opacity-[0.8]">ACCOUNT</span>
                 </p>
               </div>
-              <h1 className="font-poppins text-4xl font-semibold leading-10 pt-2 text-white pb-5 ">
-                The Next <span className="headingSpan">Generation</span> Payment
-                Method.
-              </h1>
+              <div className="hero-heading">
+                <h1 className="font-poppins text-4xl font-semibold leading-10 pt-2 text-white pb-5 ">
+                  The Next <span className="headingSpan">Generation</span>{" "}
+                  Payment Method.
+                </h1>
+                <div className="getStarted  border-2 border-[#33BBCF] rounded-full w-[130px] h-[130px] bg-transparent py-[39px] cursor-pointer mt-6 mx-auto">
+                  <p className="text-[#5CE1E6] text-center">
+                    Get
+                    <RxArrowTopRight className="inline-block text-white ml-1 text-xl" />
+                    <p>Started</p>
+                  </p>
+                </div>
+              </div>
+
               <p className="text-white opacity-[0.7] font-poppins font-normal text-[10px] leading-5 hero-des">
                 Our team of experts uses a methodology to identify the credit
                 cards most likely to fit your needs. We examine annual
                 percentage rates, annual fees.
               </p>
-              <div className="getStarted  border-2 border-[#33BBCF] rounded-full w-[130px] h-[130px] bg-transparent py-[39px] cursor-pointer mt-6 mx-auto">
+              <div className="getStarted2  border-2 border-[#33BBCF] rounded-full w-[130px] h-[130px] bg-transparent py-[39px] cursor-pointer mt-6 mx-auto">
                 <p className="text-[#5CE1E6] text-center">
                   Get
                   <RxArrowTopRight className="inline-block text-white ml-1 text-xl" />
@@ -38,6 +48,7 @@ const Hero = () => {
                 </p>
               </div>
             </div>
+
             <div className="right relative">
               <div className="all-card">
                 <div className="card1 absolute top-16 left-0 z-30">
@@ -58,24 +69,26 @@ const Hero = () => {
         </div>
 
         <div className="container">
-          <div className="user text-white flex flex-col justify-center items-center mt-10">
+          <div className="user text-white  justify-between mt-10">
             <div className="inslight">
-              <div className="active flex items-center flex-col">
-                <p className="text-3xl font-poppins font-semibold text-white">
+              <div className="active flex items-center">
+                <p className="text-3xl font-poppins font-semibold text-white user-heading">
                   3800+
                 </p>
                 <p className="useerTitle font-poppins text-xl font-normal">
                   User Active
                 </p>
               </div>
-              <div className="company flex items-center flex-col py-3">
-                <p className="text-3xl font-poppins font-semibold text-white">
+              <div className="w-[2px] h-4 bg-[#ffffff93] line"></div>
+              <div className="company flex items-center  py-3">
+                <p className="text-3xl font-poppins font-semibold text-white user-heading">
                   230+
                 </p>
                 <p className="useerTitle">TRUSTED BY COMPANY</p>
               </div>
-              <div className="Transaction flex items-center flex-col">
-                <p className="text-3xl font-poppins font-semibold text-white">
+              <div className="w-[2px] h-4 bg-[#ffffff93] line"></div>
+              <div className="Transaction flex items-center ">
+                <p className="text-3xl font-poppins font-semibold text-white user-heading">
                   $230M+
                 </p>
                 <p className="useerTitle"> TRANSACTION</p>
